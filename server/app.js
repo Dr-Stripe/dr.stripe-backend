9af4d892691dc4d86c3f1323aa4cf22e625f4abd
Tanner Haberl <thaberl13@gmail.com>
2020-09-23 18:01:43 +0900

    updated stripe server

--- server/app.js
+++ server/app.js
@@ -35,42 +35,11 @@ const setupServer = () => {
       }).then(function(result){
         res.json({success: true, message: 'ok'})
       })
-      // if ( something ){
-      //   res.status(200).json({update: something})
-      // } else {
-      //   res.status(404).json({message:"Not found"})
-      // }
     } catch (err) {
       res.status(500).json({message: "Error updating new post", error:err})
   
    }
-  // });
 
-  // try, post ?
-  // app.post("/visits/:patient_id",async(req, res) => {
-  //   const {patient_id} = req.params;
-  //   const changes = req.body;
-  //   changes["visit_date"]=moment().format('L')
-  //   console.log(changes)
-    
-    //   "symptoms": "Azuma",
-    //   "doctor": "Yamada",
-    //   "paid": false,
-    //   "hospital_name": "Tokyo clinic"
-    // }]
-  //   res.json(changes)
-  //   console.log(changes)
-  //   try {
-  //     console.log(changes)
-  //     db.table("visits").insert(
-  //       changes
-  //     )//.into("visits")
-      
-  //     res.json(changes)
-  //   } catch (err) {
-  //   //   res.status(500).json({message: "Error updating new post", error:err})
-    
-  //  }
   });
 
  
@@ -119,7 +88,6 @@ const setupServer = () => {
   });
 
 
-  
   return app;
 };
 
