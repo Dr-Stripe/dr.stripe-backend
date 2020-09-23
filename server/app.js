72147955c252f1a80eb5fabcc91b0bafedeadc1a
Hyong <hyongguk1984@gmail.com>
2020-09-23 17:43:24 +0900

    made insert

--- server/app.js
+++ server/app.js
@@ -9,38 +9,68 @@ const setupServer = () => {
   app.use(cors());
   app.use(express.json());
   app.use(bodyParser.json());
-
-
-  // try, post ?
   app.post("/visits/:patient_id",async(req, res) => {
     const {patient_id} = req.params;
-    const changes = req.body;
-    changes["date_visit"]=moment().format('L')
-    console.log(changes)
-    // 
-    //   "symptoms": "Azuma",
-    //   "doctor": "Yamada",
-    //   "paid": false,
-    //   "hospital_name": "Tokyo clinic"
+    // {
+    //   "patient_id": req.body.patient_id,
+    //    "visit_date": moment().format('L'),
+    //   "treatment": req.body.treatment,
+    //   "symptoms": req.body.symptoms,
+    //   "doctor": req.body.doctor,
+    //   "paid": req.body.paid,
+    //   "hospital_name": req.body.hospital_name
     // }]
     //res.json(changes)
-    //console.log(changes)
     try {
-      db.select().table("visits").insert(
-        {
-          patient_id: changes.patient_id
-        }
-      );
+      await db.select().table('visits').where('patient_id', req.params.patient_id).insert({
+        "patient_id": req.body.patient_id,
+         "visit_date": moment().format('L'),
+        "treatment": req.body.treatment,
+        "symptoms": req.body.symptoms,
+        "doctor": req.body.doctor,
+        "price": req.body.price,
+        "paid": req.body.paid,
+        "hospital_name": req.body.hospital_name,
+        "medicine": req.body.medicine
+      }).then(function(result){
+        res.json({success: true, message: 'ok'})
+      })
       // if ( something ){
       //   res.status(200).json({update: something})
       // } else {
       //   res.status(404).json({message:"Not found"})
       // }
-      console.log(changes)
-      res.json(changes)
     } catch (err) {
-    //   res.status(500).json({message: "Error updating new post", error:err})
+      res.status(500).json({message: "Error updating new post", error:err})
+  
    }
+  // });
+
+  // try, post ?
+  // app.post("/visits/:patient_id",async(req, res) => {
+  //   const {patient_id} = req.params;
+  //   const changes = req.body;
+  //   changes["visit_date"]=moment().format('L')
+  //   console.log(changes)
+    
+    //   "symptoms": "Azuma",
+    //   "doctor": "Yamada",
+    //   "paid": false,
+    //   "hospital_name": "Tokyo clinic"
+    // }]
+  //   res.json(changes)
+  //   console.log(changes)
+  //   try {
+  //     console.log(changes)
+  //     db.table("visits").insert(
+  //       changes
+  //     )//.into("visits")
+      
+  //     res.json(changes)
+  //   } catch (err) {
+  //   //   res.status(500).json({message: "Error updating new post", error:err})
+    
+  //  }
   });
 
  
